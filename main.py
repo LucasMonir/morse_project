d7ea7dd86addef4d0754559cb5a5730fddf0a8ea
LucasMonir <lucasmonir@hotmail.com>
2026-01-27 13:32:00 -0300

    feat: Add source file loader

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from pathlib import Path
-from modules.loader import  load_morse;
+from modules.loader import Loader;
+from tkinter import Tk;
+from tkinter.filedialog import askopenfilename;
 
-morse = {}
+morse_dict = {};
 base_dir = Path(__file__).parent;
 morse_path = base_dir / "resources" / "morse.json";
 
 def main():
-    morse = load_morse(morse_path);
+    Tk().withdraw();
+    morse_dict =   Loader.load_morse(morse_path);
+
+    source_file_path = askopenfilename();
+    source_text = Loader.load_text_to_convert(source_file_path);
 
 if __name__ == '__main__':
-   main()
+   main();
--- modules/loader.py
+++ modules/loader.py
@@ -1,18 +1,28 @@
 import json;
 from pathlib import Path;
 
+class Loader():
 
-def load_morse(path):
-    try:
-        with open(path, "r") as morse_file:
-            morse = json.load(morse_file)
+    def load_morse(path):
 
-            if not morse:          
-                raise ValueError('Morse dictionary is empty or invalid')
+            with open(path, "r") as morse_file:
+                morse = json.load(morse_file)
 
-            print('Morse dictionary loaded');
+                if not morse:          
+                    raise ValueError('Morse dictionary is empty or invalid')
 
-            return morse;
-    except:
-        print(f'Failed loading morse dictionary at directory: {path}');
- 
+                print('Morse dictionary loaded!');
+
+                return morse;
+            print(f'Failed loading morse dictionary at directory: {path}, error: {ex}');
+
+    def load_text_to_convert(path):
+            with open(path, "r") as text:
+                content = text.read();
+
+                if not content:
+                    raise ValueError("Source text file is empty");
+                
+                print('Source text file loaded!')
+                return content;
+            
